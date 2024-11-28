d310759c9e8a8db12fa43150a1ce56dfe976b0e8
oppenheimm <tommibragi03@gmail.com>
2024-11-28 17:16:40 +0000

    meh

--- office/findItemCategoryCode.py
+++ office/findItemCategoryCode.py
@@ -1,7 +1,8 @@
 from openpyxl import load_workbook
 
+
 def check_cell_value(item_number):
-    existing_file = 'retailItemList.xlsx'
+    existing_file = "retailItemList.xlsx"
     wb = load_workbook(existing_file)
     ws = wb.active
 
@@ -14,19 +15,16 @@ def check_cell_value(item_number):
                 # Get item category code, or set default if empty
                 itemCategoryCode = ws.cell(row=row_number, column=9).value
                 if itemCategoryCode is None:
-                    itemCategoryCode = 'itemCategoryNotFound'
+                    itemCategoryCode = "itemCategoryNotFound"
 
                 # Get retail product code, or set default if empty
                 retailProductCode = ws.cell(row=row_number, column=10).value
                 if retailProductCode is None:
-                    retailProductCode = 'retailProductNotFound'
+                    retailProductCode = "retailProductNotFound"
 
                 wb.close()  # Close the workbook
                 return itemCategoryCode, retailProductCode
 
     # If item number is not found
     wb.close()
-    return 'itemCategoryNotFound', 'retailProductNotFound'
-
-
-
+    return "itemCategoryNotFound", "retailProductNotFound"

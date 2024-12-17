a4afc03770be9294a66d0dd3e3780ce8e6f2b15b
Gu-wop <aronkkristjansson@gmail.com>
2024-12-17 17:40:00 +0000

    Bæti við announcement tab í innranet

--- innranet/innranetBestseller/frontend/urls.py
+++ innranet/innranetBestseller/frontend/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('NOOS', views.index),
     path('bonus', views.index),
     path('login/', views.loginn),
+    path('create/', views.create_announcement, name='create_announcement'),
+    path('edit/<int:pk>/', views.edit_announcement, name='edit_announcement'),
+    path('delete/<int:pk>/', views.delete_announcement, name='delete_announcement'),
 ]
--- innranet/innranetBestseller/frontend/views.py
+++ innranet/innranetBestseller/frontend/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from .models import Announcement
+from .forms import AnnouncementForm
 
 
 @login_required(login_url='/login')
 def index(request, *args, **kwargs):
-    return render(request, 'frontend/index.html')
+    announcements = Announcement.objects.all().order_by('-created_at')
+    return render(request, 'frontend/index.html', {'announcements': announcements})
 
 def loginn(request):
     if request.method=='POST':
@@ -21,3 +24,33 @@ def loginn(request):
             return redirect('/login')
     
     return render(request, 'login.html')
+
+def create_announcement(request):
+    if request.method == 'POST':
+        form = AnnouncementForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = AnnouncementForm()
+    return render(request, 'create_announcement.html', {'form': form})
+
+# Edit Announcement
+def edit_announcement(request, pk):
+    announcement = get_object_or_404(Announcement, pk=pk)
+    if request.method == 'POST':
+        form = AnnouncementForm(request.POST, instance=announcement)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AnnouncementForm(instance=announcement)
+    return render(request, 'edit_announcement.html', {'form': form})
+
+# Delete Announcement
+def delete_announcement(request, pk):
+    announcement = get_object_or_404(Announcement, pk=pk)
+    if request.method == 'POST':
+        announcement.delete()
+        return redirect('home')
+    return render(request, 'confirm_delete.html', {'announcement': announcement})

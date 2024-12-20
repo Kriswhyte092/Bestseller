591df064f4716511e48122d173998f644a5901c3
Gu-wop <aronkkristjansson@gmail.com>
2024-12-20 12:09:01 +0000

    Update views.py

--- innranet/innranetBestseller/frontend/views.py
+++ innranet/innranetBestseller/frontend/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 
-# @login_required(login_url='/login')
+@login_required(login_url='/login')
 def index(request, *args, **kwargs):
     return render(request, "frontend/index.html")
 

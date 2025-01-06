b6d1f682cf0720e3912f02be33cfcdf1808fa990
Gu-wop <aronkkristjansson@gmail.com>
2025-01-06 14:41:44 +0000

    lagfæra staff view

--- innranet/innranetBestseller/frontend/views.py
+++ innranet/innranetBestseller/frontend/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
 
 @login_required(login_url='/login')
+@admin_only
 def index(request, *args, **kwargs):
     return render(request, "frontend/index.html")
 
 
+@login_required(login_url='/login')
 def staff(request):
     return render(request, "frontend/staff.html")
 
@@ -28,9 +30,6 @@ def loginn(request):
             return redirect("/login")
 
     return render(request, "frontend/login.html")
-
-
-from django.contrib.auth import logout
 
 
 @login_required(login_url="/login")

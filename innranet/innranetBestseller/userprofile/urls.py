3d1063e693e9cb1a4a9fcbeb8a637ccb20926554
oppenheimm <tommibragi03@gmail.com>
2025-01-09 13:38:55 +0000

    laun format fixed

--- innranet/innranetBestseller/userprofile/urls.py
+++ innranet/innranetBestseller/userprofile/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile/", views.profile_view, name="profile"),
+    path("", views.profile_view, name="profile"),
     # Add other URL patterns here
 ]

fcbd76805b03d25cf608246f7b2c38cc644e6845
Gu-wop <aronkkristjansson@gmail.com>
2025-01-27 20:26:49 +0000

    Update import_products.py

--- innranet/innranetBestseller/noos/management/commands/import_products.py
+++ innranet/innranetBestseller/noos/management/commands/import_products.py
@@ -3,12 +3,19 @@ from noos.import_products import import_product, JJ_noos_barcodes
 from noos.product_apis import fashion_cloud_api
 from tqdm import tqdm
 import logging
+import time
 
-logging.basicConfig(filename='log/import_products.log', level=logging.ERROR, 
-                    format='%(asctime)s %(levelname)s:%(message)s')
+"""
+This is a command that uses the Fashion Cloud API to import products into the database.
+"""
 
 class Command(BaseCommand):
     help = 'Import products from Fashion Cloud API'
+    
+    start_time = time.time()
+    # Set up logging
+    logging.basicConfig(filename='log/import_products.log', level=logging.ERROR, 
+                    format='%(asctime)s %(levelname)s:%(message)s')
 
     def handle(self, *args, **kwargs):
         for code in tqdm(JJ_noos_barcodes, desc="Processing products"):
@@ -24,3 +31,5 @@ class Command(BaseCommand):
                 logging.error(f"Error processing barcode {code}: {e}")
                 self.stdout.write(self.style.ERROR(f"Error processing barcode {code}: {e}"))
                 continue
+        elapsed_time = time.time() - self.start_time
+        self.stdout.write(self.style.SUCCESS(f"Successfully imported products in {elapsed_time:.2f} seconds"))

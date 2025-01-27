ce8316cd54be2faaf5aa3f182711ef0d0ac0b84f
Gu-wop <aronkkristjansson@gmail.com>
2025-01-27 20:26:25 +0000

    Update models.py

--- innranet/innranetBestseller/noos/models.py
+++ innranet/innranetBestseller/noos/models.py
@@ -7,10 +7,28 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     itemNo = models.CharField(max_length=255)
     product_description = models.TextField(default="No description added")
+    
+    def get_variant(self, variant_code):
+        # Filter variants directly by the BarcodeNo within the related color variants
+        variant = Variant.objects.filter(colorVariant__product=self, BarcodeNo=variant_code).first()
+        if not variant:
+            return None  # Explicitly return None for clarity
+        return variant
 
     def __str__(self):
-        return f"{self.name} ({self.itemNo})"
+        color_variants_info = [
+            f"{color_variant.colorName} ({color_variant.variants.count()} variants)"
+            for color_variant in self.color_variants.all()
+        ]
 
+        color_variants_str = "\n".join(color_variants_info)
+
+        return (
+            f"{self.name} ({self.itemNo})"
+            f" - {len(self.color_variants.all())} color variants\n"
+            # f"{color_variants_str}"
+        )
+        
 class colorVariant(models.Model):
     product = models.ForeignKey(
         Product, 
@@ -38,7 +56,7 @@ class Variant(models.Model):
     length = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return f"[{self.BarcodeNo}] {self.colorVariant.colorName}"
+        return f"{self.colorVariant.product.name} - {self.colorVariant.colorName} {self.size} [Barcode: {self.BarcodeNo}] "
 
 class Store(models.Model):
     store_name = models.CharField(max_length=255)
@@ -50,6 +68,8 @@ class Inventory(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+    class Meta:
+            unique_together = ('store', 'variant')  # Ensures one inventory record per store-product pair
 
     def __str__(self):
         return f"{self.store} / {self.variant} / Qty: {self.quantity}"
